88322142cdccfce0bf98ef579d763f2c47bf929e
bt96 <bishttarun96@gmail.com>
2016-06-17 11:59:28 +0000

    added ui-router concept

--- app.route.js
+++ app.route.js
@@ -1,26 +1,34 @@
-var LEEDOnApp = angular.module('LEEDOnApp', ['ngRoute']);
+var LEEDOnApp = angular.module('LEEDOnApp', ['ui.router']);
 
 // configure our routes
-LEEDOnApp.config(function($routeProvider) {
-    $routeProvider
-    .when('/', {
-        templateUrl : 'apps/components/home/homeView.html',
-        controller  : 'homeController'
+LEEDOnApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise("/home");
+
+    $stateProvider
+    .state("homeState", {
+        url: "/home",
+        templateUrl: "apps/components/home/homeView.html",
+        controller : "homeController"
     })
-    .when('/login', {
-        templateUrl : 'apps/components/login/loginView.html',
-        controller  : 'loginController'
+    .state("loginState", {
+        url: "/login",
+        templateUrl: "apps/components/login/loginView.html",
+        controller : "loginController"
     })
-    .when('/dashboard', {
-        templateUrl : 'apps/components/dashboard/dashboardView.html',
-        controller  : 'dashboardController'
-    })
-    .when('/score', {
-        templateUrl : 'apps/components/score/scoreView.html',
-        controller  : 'scoreController'
-    })
-    .otherwise({
-        // default page
-        redirectTo: '/'
-    });
+    .state("projectState", {
+        url: "/project",
+        templateUrl: "apps/components/project/projectView.html",
+        controller : "projectController"
+     })
+    .state("dashboardState", {
+        url: "/dashboard/score",
+        templateUrl: "apps/components/dashboard/dashboardView.html",
+        controller : "dashboardController"
+     });
+    // .state("dashboardState.scoreState", {
+    //     url: "/score",
+    //     templateUrl: "apps/components/score/scoreView.html",
+    //     controller : "scoreController"
+    // });
 });
---
+++ apps/components/project/projectController.js
@@ -0,0 +1,52 @@
+LEEDOnApp.controller('projectController', function($rootScope, $scope, $http) {
+	$rootScope.header = 'Projects';
+	$rootScope.main_appClass = 'overflow_y_scroll';
+	$rootScope.htmlLayout = 'js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths';
+	$http.get('assets/json/countriesstates.json').success(function(csJson) {
+		$http.get('assets/json/building_data.json').success(function(data) {
+			for (var i in data)
+	            {
+	                back = data[i].state;
+	                if (back == null)
+	                {
+	                    back = 'None';
+	                    data[i].state = 'None';
+	                }
+
+	                data[i].state = data[i].state.substr(2);
+	                if(data[i].state.length == 0 || back == 'None')
+	                {
+	                    data[i].state = 'None';   
+	                }
+	                else if(data[i].state.length == 2 || data[i].state.length == 1)
+	                {
+	                    if(data[i].state != data[i].country) 
+	                    {
+	                        try {
+	                            data[i].state = csJson['divisions'][data[i].country][data[i].state]
+	                        }
+	                        catch(err) {
+	                            data[i].state = data[i].state
+	                        }    
+	                    }
+	                }
+	                else
+	                {
+	                    data[i].state = back;       
+	                }
+
+	                if(data[i].city == 'None')
+	                    street = data[i].state;
+	                if(data[i].state == 'None')
+	                    street = data[i].city;
+	                if(data[i].city == 'None' && data[i].state == 'None')
+	                    street = '';
+	                else if(data[i].city != 'None' && data[i].state != 'None')
+	                    street = data[i].city + ", " + data[i].state ;
+	                data[i].location = street;
+
+	            }
+			$scope.buildings = data;
+		});
+	});
+});
