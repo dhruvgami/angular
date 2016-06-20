e4fe6a472e4011c3d08d37cba47df69d3f649c96
bt96 <bishttarun96@gmail.com>
2016-06-20 05:21:57 +0000

    added images

--- apps/components/dashboard/analysis/analysisController.js
+++ apps/components/dashboard/analysis/analysisController.js
@@ -1,5 +1,4 @@
-LEEDOnApp.controller('scoreController', function($rootScope, $scope) {
-	$rootScope.header = 'Score';
+LEEDOnApp.controller('analysisController', function($rootScope, $scope) {
+	$rootScope.header = 'Analysis';
 	$rootScope.main_appClass = '';
-	// $rootScope.htmlLayout = 'js flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface no-generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths fp-enabled';
 });
