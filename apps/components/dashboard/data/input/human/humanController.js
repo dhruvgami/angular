380e2dbf4bad4612bc71b6c28d23d2098738775a
bt96 <bishttarun96@gmail.com>
2016-06-23 09:39:57 +0000

    removed data input subsection

--- apps/components/dashboard/data/input/human/humanController.js
+++
@@ -1,8 +0,0 @@
-LEEDOnApp.controller('humanController', function($rootScope, $scope) {
-	$rootScope.main_appClass = '';
-	document.getElementById("energy_cat_color_angular").style.display    = "none";
-	document.getElementById("water_cat_color_angular").style.display     = "none";
-	document.getElementById("waste_cat_color_angular").style.display     = "none";
-	document.getElementById("human_cat_color_angular").style.display     = "block";
-	document.getElementById("transport_cat_color_angular").style.display = "none";
-});
--- apps/components/dashboard/data/input/inputController.js
+++ apps/components/dashboard/data/input/inputController.js
@@ -1,5 +1,5 @@
 LEEDOnApp.controller('inputController', function($rootScope, $scope) {
-	$rootScope.header = 'Input';
-	$rootScope.main_appClass = '';
+	$rootScope.header = 'Data Input';
 	document.getElementById("manual_img").style.display = "block";
+	$scope.selectedTab = 'energy';
 });
